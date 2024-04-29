69754bea88e04582dd79192b27985340f7d8554c
donghao <is_simple@163.com>
2024-04-29 09:38:12 +0800

    feat: control by h5

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2024/4/23 9:53 PM
-@Auth ： donghao
-"""
-import machine
+import network
 import time
+import socket
 
-# 设置 GPIO 引脚号
-pin_number = 16  # 这里假设使用 GPIO 0
 
-# 初始化 GPIO 引脚
-pin = machine.Pin(pin_number, machine.Pin.OUT)
+def web_page():
+    html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+            <body><h1>Hello World</h1></body></html>
+         """
+    return html
 
-# 将 GPIO 引脚设置为高电平
-pin.value(1)
-time.sleep(10)  # 等待一秒
 
-# 将 GPIO 引脚设置为低电平
-pin.value(0)
-time.sleep(1)  # 等待一秒
+# if you do not see the network you may have to power cycle
+# unplug your pico w for 10 seconds and plug it in again
+def ap_mode(ssid, password):
+    """
+        Description: This is a function to activate AP mode
 
-# 最后记得释放 GPIO 引脚
-pin.deinit()
+        Parameters:
+
+        ssid[str]: The name of your internet connection
+        password[str]: Password for your internet connection
+
+        Returns: Nada
+    """
+    # Just making our internet connection
+    ap = network.WLAN(network.AP_IF)
+    ap.config(essid=ssid, password=password)
+    ap.active(True)
+
+    while ap.active() == False:
+        pass
+    print('AP Mode Is Active, You can Now Connect')
+    print('IP Address To Connect to:: ' + ap.ifconfig()[0])
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # creating socket object
+    s.bind(('', 80))
+    s.listen(5)
+
+    while True:
+        conn, addr = s.accept()
+        print('Got a connection from %s' % str(addr))
+        request = conn.recv(1024)
+        print('Content = %s' % str(request))
+        response = web_page()
+        conn.send(response)
+        conn.close()
+
+
+ap_mode('pihahaha','dt20171008.')
